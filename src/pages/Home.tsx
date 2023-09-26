e136536af18274562bb69846a6c53a4ce92cc90c
brian <90752841+wokbjso@users.noreply.github.com>
2023-09-26 17:36:09 +0900

    Feat:Color 및 Typeface theme 파일에 정리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <A
+      style={{
+        fontSize: "5rem",
+      }}
+    >
+      Home
+    </A>
+  );
 }
+
+const A = styled.div`
+  ${(props) => props.theme.fontStyles.headLine1}
+`;
