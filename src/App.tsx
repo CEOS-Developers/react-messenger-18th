673e2708207151dccb22f803225b8adfa9f36db9
silver <ekdldkaa@naver.com>
2023-09-29 22:50:00 +0900

    [chore] package 의존설정 변화

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import ChatRoom from './pages/ChatRoom';
 import React from 'react';
 import styled from 'styled-components';
+import theme from './style/theme';
 
 function App() {
   return (
     <Wrapper>
-      <ChatRoom id={0} />
+      <Box>
+        <ChatRoom id={0} />
+      </Box>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Box = styled.div`
   width: 23.4375rem;
   height: 50.75rem;
   border-radius: 1rem;
   background: var(--white, #fff);
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: ${theme.colors.g2};
 `;
 
 export default App;
