e386d5f034f19b85f28d77281d45b2f1bee5b44d
silver <ekdldkaa@naver.com>
2023-09-29 18:23:32 +0900

    [feat] input컴포넌트

--- src/components/ChatInputBar.tsx
+++ src/components/ChatInputBar.tsx
@@ -1,33 +1,96 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import theme from '../style/theme';
 import PlusIcon from '../static/PlusIcon';
 import SmileIcon from '../static/SmileIcon';
 import HashIcon from '../static/HashIcon';
+import SendIcon from '../static/SendIcon'; // SendIcon import
 
 const ChatInputBar = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [isInputFocused, setInputFocused] = useState(false);
+
+  const handleInputFocus = () => {
+    setInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    if (inputRef.current) {
+      if (inputRef.current.value.trim() !== '') {
+        setInputFocused(true);
+      } else {
+        setInputFocused(false);
+      }
+    }
+  };
+
+  const handleIconClick = () => {
+    console.log('보내기');
+  };
+
   return (
     <Wrapper>
       <PlusIcon />
       <InputBox>
-        <input />
+        <Input
+          ref={inputRef}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+        />
         <SmileIcon />
-        <HashIcon />
+        {isInputFocused ? (
+          <Button onClick={handleIconClick}>
+            <SendIcon />
+          </Button>
+        ) : (
+          <HashIcon />
+        )}
       </InputBox>
     </Wrapper>
   );
 };
 
+const Button = styled.button`
+  border: none;
+  outline: none;
+  padding: 0px;
+  margin: 0px;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 75%;
+  border: none;
+  outline: none;
+  background-color: ${theme.colors.g2};
+  margin-left: 11px;
+  margin-top: 1px;
+  margin-bottom: 1px;
+`;
+
 const InputBox = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  width: 19rem;
+  height: 2.25rem;
   border-radius: 1rem;
+  border: 0.24px solid ${theme.colors.g4};
+  background: ${theme.colors.g2};
+  padding-right: 10px;
 `;
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 12px;
 `;
 
 export default ChatInputBar;
