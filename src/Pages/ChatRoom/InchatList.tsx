0abbd8805044b30b214196c86d6790021e5f6ad4
김지원 <jibom0507@naver.com>
2023-09-29 20:48:17 +0900

    [feat]:채팅 보낼 때 스크롤바 하단 로딩

--- src/Pages/ChatRoom/InchatList.tsx
+++ src/Pages/ChatRoom/InchatList.tsx
@@ -1,29 +1,36 @@
 import { styled } from "styled-components";
 import Chat from "./Chat";
+import React, { useEffect, useRef } from "react";
 
 type InchatListProps = {
   chat: Array<{ value: string; id: number; sender: boolean; date: string }>;
 };
 
 const InchatList: React.FC<InchatListProps> = ({ chat }) => {
+  const chatListRef = useRef<HTMLDivElement>(null);
+
+  // chat 배열이 업데이트될 때 스크롤을 맨 아래로 이동
+  useEffect(() => {
+    if (chatListRef.current) {
+      chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
+    }
+  }, [chat]);
+
   return (
     <Wrapper>
-      <Container>
-        <Container id="chat-container">
-          {chat.map((message, index) => (
-            <Chat
-              value={message.value}
-              id={message.id}
-              sender={message.sender}
-              date={message.date}
-              key={message.id}
-              showDate={
-                index === chat.length - 1 ||
-                message.date !== chat[index + 1].date
-              }
-            />
-          ))}
-        </Container>
+      <Container ref={chatListRef} id="chat-container">
+        {chat.map((message, index) => (
+          <Chat
+            value={message.value}
+            id={message.id}
+            sender={message.sender}
+            date={message.date}
+            key={message.id}
+            showDate={
+              index === chat.length - 1 || message.date !== chat[index + 1].date
+            }
+          />
+        ))}
       </Container>
     </Wrapper>
   );
