a03baf849ecdd2b034c23b575468c3a4ef780834
brian <90752841+wokbjso@users.noreply.github.com>
2023-09-29 22:38:20 +0900

    Feat:정의된 페이지 라우트 이외의 라우트 접근 시 홈 화면으로 Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import PhoneLayout from "./components/iPhone/PhoneLayout";
 import ChatList from "./pages/ChatList";
 import FriendsList from "./pages/FriendsList";
@@ -16,6 +21,7 @@ function App() {
           <Route path="/my-profile" element={<MyProfile />} />
           <Route path="/chatroom/main/:id" element={<ChatRoom />} />
           <Route path="/chatroom/sub/:id" element={<ChatRoom />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </Router>
